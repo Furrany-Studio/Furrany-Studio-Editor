a3c66c80df30e0d27963198e81564b5041b6d08f
Maxime66410 <max66410@hotmail.fr>
2021-07-15 18:47:24 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+#include <windows.h>
 
-int main(int argc, char **argv)
+int main(int nCmdShow)
 {
-    std::cout << "Hello world !\n";
+    WNDCLASSEX windowClass = {sizeof(WNDCLASSEX)};
 
-    return 0;
+    windowClass.lpszClassName = "mainWindow";
+    windowClass.style = CS_OWNDC;
+    windowClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+    RegisterClassEx(&windowClass);
+
+    HWND windowsHandle = CreateWindowEx(
+            0,
+            windowClass.lpszClassName,
+            "Furrany Studio",
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+            NULL,
+            NULL,
+            windowClass.hInstance,
+            NULL
+            );
+
+    if(windowsHandle == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(windowsHandle, nCmdShow);
+
 }
