41d3775779d391ff84facf354ab8fa623adfe4db
Maxime66410 <max66410@hotmail.fr>
2023-10-11 00:03:57 +0200

    Import GL / Make SImple triangle

--- OpenGLBranch/FurranyEditorOpenGL/FurranyEditorOpenGL/FurranyEditorOpenGL.cpp
+++ OpenGLBranch/FurranyEditorOpenGL/FurranyEditorOpenGL/FurranyEditorOpenGL.cpp
@@ -1,12 +1,29 @@
-#include <GL/>
+#include "dependencies/include/GL/glew.h"
 #include "dependencies/include/GLFW/glfw3.h"
+
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout(location = 0) in vec3 aPos;
+    void main() {
+        gl_Position = vec4(aPos, 1.0);
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+)";
 
 int main() {
     if (!glfwInit()) {
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World !", nullptr, nullptr);
+    // Crée la fenêtre GLFW
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test Triangle", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -14,12 +31,55 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK) {
+        return -1;
+    }
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,  // Sommet 1
+         0.5f, -0.5f, 0.0f,  // Sommet 2
+         0.0f,  0.5f, 0.0f   // Sommet 3
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
-        // Dessine ton contenu OpenGL ici
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VBO);
+    glDeleteVertexArrays(1, &vertexShader);
+    glDeleteVertexArrays(1, &fragmentShader);
 
     glfwTerminate();
     return 0;
